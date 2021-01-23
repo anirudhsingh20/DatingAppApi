01131a0932c924d36f80617196832d86a4f9d20c
Anirudh Singh <anirudh.singh@pojolabs.in>
2021-01-24 01:29:17 +0530

    Add new controller

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,37 @@
+﻿using DatingApp.Data;
+using DatingApp.Dtos;
+using DatingApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace DatingApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _repo;
+        public AuthController(IAuthRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserDto userDto)
+        {
+            userDto.Username = userDto.Username.ToLower();
+            if (await _repo.UserExists(userDto.Username))
+                return BadRequest("User Already Exists");
+
+            var userToCreate = new User
+            {
+                Username = userDto.Username
+            };
+
+            var createdUser = _repo.Register(userToCreate, userDto.Password);
+
+            return StatusCode(201);
+
+        }
+    }
+}
